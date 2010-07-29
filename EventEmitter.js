036bd696fe587f63a1803ebbe00e09a4ce07e97d
Liang Zeng <lelandtseng@gmail.com>
2010-07-29 08:50:38 +0800

    EventEmitter version : 0.0.2

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,7 +1,22 @@
 function EventEmitter(){
-    var map = new Object ;    
-    var listenernum = new Object;    
+    var map = new Object;
+    var listenernum = new Object;
+    
+    
     this.on = function(eventname, listener){
+        var self = this;
+        if (arguments.length != 2) {
+            self.emit("error", "on() method must have 2 arguments.");
+            return;
+        }
+        if (typeof eventname != "string") {
+            self.emit("error", "on() first argument must String type.");
+            return;
+        }
+        if (!(listener instanceof Function)) {
+            self.emit("error", "on() second argument must Function type.");
+            return;
+        }
         if (map[eventname]) 
             ;
         else 
@@ -13,19 +28,62 @@ function EventEmitter(){
             listenernum[eventname] = "1";
         var num = listenernum[eventname] = parseInt(listenernum[eventname]) + 1 + "";
         map[eventname][num] = listener;
+        self.emit("newListener", eventname, listener);
         return num;
     }
     this.removeListener = function(eventname, listenernum){
+        var self = this;
+        if (arguments.length != 2) {
+            self.emit("error", "removeListener() method must have 2 arguments.");
+            return;
+        }
+        if (typeof eventname != "string") {
+            self.emit("error", "removeListener() first argument must String type.");
+            return;
+        }
+        if (!(listener instanceof Function)) {
+            self.emit("error", "removeListener() second argument must Function type.");
+            return;
+        }
         delete map[eventname][listenernum];
     }
     this.removeAllListener = function(eventname){
+        var self = this;
+        if (arguments.length != 1) {
+            self.emit("error", "removeAllListener() method must have 1 arguments.");
+            return;
+        }
+        if (typeof eventname != "string") {
+            self.emit("error", "removeAllListener() first argument must String type.");
+            return;
+        }
         delete map[eventname];
     }
     this.listeners = function(eventname){
-        return map[eventname];
+        var self = this;
+        if (arguments.length != 1) {
+            self.emit("error", "listeners() method must have 1 arguments.");
+            return;
+        }
+        if (typeof eventname != "string") {
+            self.emit("error", "listeners() first argument must String type.");
+            return;
+        }
+        var result = new Array;
+        for (var key in map[eventname]) 
+            result.push(map[eventname][key]);
+        return result;
     }
     this.emit = function(eventname){
-    
+        var self = this;
+        if (!(arguments.length >= 1)) {
+            self.emit("error", "emit() method must have 1 arguments.");
+            return;
+        }
+        if (typeof eventname != "string") {
+            self.emit("error", "emit() first argument must String type.");
+            return;
+        }
         var args = new Array;
         
         for (var i = 0; i < arguments.length - 1; i++) {
@@ -37,4 +95,11 @@ function EventEmitter(){
             func.apply(this, args);
         }
     }
+    
+    
+    
+    this.on("error", function(exception){
+        alert(exception);
+    });
+    
 }
